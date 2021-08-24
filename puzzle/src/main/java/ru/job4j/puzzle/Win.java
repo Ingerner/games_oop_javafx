2c36d65b4b54530feec0fbb3ab01d3102a9c0484
Олег <korotkiy.o@yandex.ru>
2021-08-24 23:26:20 +0300

    Сработала строка

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,6 +3,18 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = true;
+        for(int indexStr=0; indexStr<board.length; indexStr++) {
+            for(int indexStl=0; indexStl<board[indexStr].length; indexStl++) {
+                if(board[indexStr][indexStl] != 1){
+                    rsl = false;
+                    break;
+                }
+                if(indexStl == board[indexStr].length - 1) {
+                    return true;
+                }
+            }
+        }
+        //boolean rsl = true;
         return rsl;
     }
 }

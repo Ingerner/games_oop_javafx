46600686723fbbfe184e40c6a8835e247ea0f6da
Thinkpad <korotkiy.o@yandex.ru>
2021-10-11 23:42:47 +0300

    Test 1

---
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.chess.firuges.black;
+
+import org.junit.Test;
+import ru.job4j.chess.firuges.Cell;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class BishopBlackTest {
+
+    @Test
+    public void position() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.A1);
+       Cell rez = bishopBlack.position();
+        assertThat(rez, is(Cell.A1));
+    }
+}

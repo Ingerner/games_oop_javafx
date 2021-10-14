0037946be010edad5c85fcd42378c79f24856a25
Oleg <korotkiy.o@yandex.ru>
2021-10-14 16:51:48 +0300

    logic test

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -7,17 +7,13 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class LogicTest {
 
-    @Ignore
+    //@Ignore
     @Test
     public void move()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.H6);
+        logic.move(Cell.C1, Cell.G5);
     }
 
-    @Test
-    public void testMove() {
-
-    }
 }
